1cd708d0f767d5432ded2aae32c98c66244d168f
moau16 <ag.man@live.se>
2019-01-11 17:15:41 +0100

    uppdaterad¨

--- src/CMDInput.java
+++ src/CMDInput.java
@@ -1,28 +1,43 @@
 public class CMDInput {
     public static void main(String[] args) {
-        if(args.length == 0) {
+        if (args.length == 0) {
             System.out.println("Programmet vet inte vad det ska göra! Avbryter...");
         }
-        if(args.length == 1) {
+        if (args.length == 1) {
             System.out.println("Så du säger " + args[0] + ".");
         }
+        try {
+            if (args.length == 2) {
+                int result = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+                System.out.print(args[0] + " + " + args[1] + " = ");
+                System.out.println(result);
 
-        if(args.length == 2) {
-            int result = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
-            System.out.println(result);
+            }
 
+        } catch (NumberFormatException result) {
+            System.out.println("Du måste använda siffror.");
         }
 
-        if(args.length == 3) {
+
+        if (args.length == 3) {
             System.out.println(args[2] + " " + args[1] + " " + args[0]);
         }
 
-        if(args.length >= 4){
+        if (args.length >= 4) {
             System.out.println("Jag orkar inte, jag stänger ner");
         }
 
-        for(int i = 0 ; i < args.length ; i++){
-            System.out.println(args[i]);
+        System.out.println();
+
+        System.out.print("Dina argument: ");
+
+        if(args.length == 0){
+            System.out.print("du har inga argument.");
         }
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.print(args[i] + " ");
+        }
+
     }
 }
--- src/GUIDesign.java
+++ src/GUIDesign.java
@@ -1,2 +1,10 @@
+import javax.swing.*;
+
 public class GUIDesign {
+    private JButton openButton;
+    private JButton saveButton;
+    private JButton cryptButton;
+    private JButton decryptButton;
+    private JTextArea textArea1;
+    private JTextArea textArea2;
 }
